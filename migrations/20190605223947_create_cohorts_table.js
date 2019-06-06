356b023a336c2e147f9c2af89cdaf31d859ae2af
azinoVo <azinovo22@gmail.com>
2019-06-05 23:37:59 -0500

    added teh foreign key code

--- migrations/20190605223947_create_cohorts_table.js
+++ migrations/20190605223947_create_cohorts_table.js
@@ -3,6 +3,7 @@
 exports.up = function(knex, Promise) {
   return knex.schema.createTable('cohorts', function(tbl) {
     tbl.increments();
+    
     tbl.string('name', 125).notNullable();
   })
 };
